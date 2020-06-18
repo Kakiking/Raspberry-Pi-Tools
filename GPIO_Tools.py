5f1697244187c76eb1ba284fbe894dc99946f7c5
Kakiking <66200818+Kakiking@users.noreply.github.com>
2020-06-18 17:27:30 +0200

    Add files via upload

--- GPIO_Tools.py
+++ GPIO_Tools.py
@@ -2,12 +2,12 @@
 
 import RPi.GPIO as GPIO
 
-class PfeiltastenHandler():
-    def __init__(parent=None):
-        ButtonUP_pin = 37
-        ButtonDOWN_pin = 33
-        ButtonLEFT_pin = 22
-        ButtonRIGHT_pin = 35
+class PfeiltastenHandler(parent=None):
+    def __init__(UP_pin=37, DOWN_pin=33, LEFT_pin=22, RIGHT_pin=35):
+        self.ButtonUP_pin = UP_pin
+        self.ButtonDOWN_pin = DOWN_pin
+        self.ButtonLEFT_pin = LEFT_pin
+        self.ButtonRIGHT_pin = RIGHT_pin
 
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(ButtonUP_pin, GPIO.IN)
@@ -15,13 +15,13 @@ class PfeiltastenHandler():
         GPIO.setup(ButtonLEFT_pin, GPIO.IN)
         GPIO.setup(ButtonRIGHT_pin, GPIO.IN)
     def check_for_input():
-        if (GPIO.input(ButtonUP_pin) == 0):
+        if (GPIO.input(self.ButtonUP_pin) == 0):
             return "up"
-        if (GPIO.input(ButtonDOWN_pin) == 0):
+        if (GPIO.input(self.ButtonDOWN_pin) == 0):
             return "down"
-        if (GPIO.input(ButtonLEFT_pin) == 0):
+        if (GPIO.input(self.ButtonLEFT_pin) == 0):
             return "left"
-        if (GPIO.input(ButtonRIGHT_pin) == 0):
+        if (GPIO.input(self.ButtonRIGHT_pin) == 0):
             return "right"
     finally:
         GPIO.cleanup()
